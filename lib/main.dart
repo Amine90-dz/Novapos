3ef368a1a300027a55fbd4ff0253321b05f64963
Amine90-dz <boussenamohamedamine@gmail.com>
2026-08-14 22:58:55 +0100

    Add SalesPage navigation and import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'products_page.dart';
+import 'sales_page.dart';
 
 void main() {
   runApp(const NovaPOSApp());
@@ -95,17 +96,21 @@ class HomePage extends StatelessWidget {
                   crossAxisSpacing: 12,
                   mainAxisSpacing: 12,
                   children: [
+                    // المبيعات
                     _MenuCard(
                       icon: Icons.point_of_sale,
                       title: 'المبيعات',
                       onTap: () {
-                        _showComingSoon(
+                        Navigator.push(
                           context,
-                          'المبيعات',
+                          MaterialPageRoute(
+                            builder: (_) => const SalesPage(),
+                          ),
                         );
                       },
                     ),
 
+                    // المخزون
                     _MenuCard(
                       icon: Icons.inventory_2,
                       title: 'المخزون',
@@ -117,6 +122,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // المنتجات
                     _MenuCard(
                       icon: Icons.checkroom,
                       title: 'المنتجات',
@@ -124,13 +130,13 @@ class HomePage extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (_) =>
-                                const ProductsPage(),
+                            builder: (_) => const ProductsPage(),
                           ),
                         );
                       },
                     ),
 
+                    // التقارير
                     _MenuCard(
                       icon: Icons.bar_chart,
                       title: 'التقارير',
@@ -142,6 +148,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // الألوان والمقاسات
                     _MenuCard(
                       icon: Icons.palette,
                       title: 'الألوان والمقاسات',
@@ -153,6 +160,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // الزبائن
                     _MenuCard(
                       icon: Icons.people,
                       title: 'الزبائن',
@@ -164,6 +172,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // ديون الزبائن
                     _MenuCard(
                       icon: Icons.account_balance_wallet,
                       title: 'ديون الزبائن',
@@ -175,6 +184,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // الموردون
                     _MenuCard(
                       icon: Icons.local_shipping,
                       title: 'الموردون',
@@ -186,6 +196,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // ديون الموردين
                     _MenuCard(
                       icon: Icons.money_off,
                       title: 'ديون الموردين',
@@ -197,6 +208,7 @@ class HomePage extends StatelessWidget {
                       },
                     ),
 
+                    // الإعدادات
                     _MenuCard(
                       icon: Icons.settings,
                       title: 'الإعدادات',
@@ -238,8 +250,7 @@ class _MenuCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         child: Center(
           child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(
                 icon,
